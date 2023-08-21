49fa61ea82d61a9d787c417b99762e5a759784c9
hayeong <hayoungju98@naver.com>
2023-08-21 13:06:35 +0900

    20230821

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Welcome from "./component/Welcome";
+import World from "./component/World";
+import Hello from "./component/hello";
+
+function App() {
+  // 변수
+  const name = "Hong";
+  let age = 30;
+  // 객체
+  const naver ={
+    name : "네이버",
+    url : "https://www.naver.com",
+    target : "_blank"
+  }
+  return (
+    <div className="App">
+      <Hello ></Hello>
+      <Welcome></Welcome>
+    
+      {/* {변수, 숫자, 문자열} 사용 가능 */}
+      <h1>{'- 이름 :'} {name}, {'- 나이 :'}{age}</h1>
+      <h1> - 이름 : {name}, - 나이 : {age}</h1>
+      {/* 객체 {객체 이름, 속성} */}
+      <h2><a href={naver.url} target={naver.target}>{naver.name}</a></h2>
+    </div>
+  );
+}
+
+export default App;
